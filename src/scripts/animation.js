6a5cf22c535e5e5c6f4cef2b1b9acdeadd38c276
IASP23 <alexanderpilataxi232002@gmail.com>
2025-02-01 18:28:28 -0500

    astro

---
+++ src/scripts/animation.js
@@ -0,0 +1,37 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".temporizer", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
+
+sr.reveal(".timer", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "-100px",
+});
+
+sr.reveal(".gallery", {
+  duration: 3000,
+  origin: "left",
+  distance: "50px",
+});
+
+sr.reveal(".dressCode", {
+  duration: 3000,
+  origin: "top",
+  distance: "50px",
+});
+
+sr.reveal(".form", {
+  duration: 3000,
+  origin: "top",
+  distance: "150px",
+});
+
+sr.reveal(".footer", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "150px",
+});
