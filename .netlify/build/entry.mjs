466b29442b14bdd4819df8dd1ea3f98f06306e43
IASP23 <alexanderpilataxi232002@gmail.com>
2025-02-02 18:59:11 -0500

    Corrections y

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CetrFWkl.mjs';
+import { manifest } from './manifest_Y-K08se_.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "7bf67e9f-bafd-4bc9-b1f6-0d4f0555cff5"
+    "middlewareSecret": "3e099d00-f031-4ea5-84cb-edb294f950c4"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
